87263aa9fe2c76348bf6d7cea87898b70982171a
Paul-js-hub <hesponpoisson@gmail.com>
2022-05-13 22:33:36 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//function takes an array returns a square of its elements
+// fix the function so that it returns correct output
+let detectBug = (array) =>{
+    let res = arra.map(arr => ar * 2);
+}
+
+console.log(detectBug([1, 2, 3, 4]))
